17326a0e14926e9e7706bd0853ec4b4f8925b45a
Tao Wen <taowen@gmail.com>
2019-01-13 16:38:53 +0800

    move struct descriptor to spi

--- src/main/java/com/jsoniter/dson/spi/StructDescriptor.java
+++ src/main/java/com/jsoniter/dson/spi/StructDescriptor.java
@@ -1,19 +1,19 @@
-package com.jsoniter.dson.codegen;
-
-import com.jsoniter.dson.spi.DsonProperty;
+package com.jsoniter.dson.spi;
 
 import java.beans.Transient;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Predicate;
 
 public class StructDescriptor {
 
     private final Class clazz;
-    public final Map<String, StructProperty> fields = new HashMap<>();
-    public final Map<String, StructProperty> methods = new HashMap<>();
+    public final Map<String, Prop> fields = new HashMap<>();
+    public final Map<String, Prop> methods = new HashMap<>();
 
     public StructDescriptor(Class clazz) {
         this.clazz = clazz;
@@ -24,7 +24,7 @@ public class StructDescriptor {
             if (Object.class.equals(field.getDeclaringClass())) {
                 continue;
             }
-            StructProperty member = new StructProperty(field);
+            Prop member = new Prop(field);
             if (Modifier.isTransient(field.getModifiers()) && member.getAnnotation(DsonProperty.class) == null) {
                 member.setAnnotation(DsonProperty.class, new DsonProperty.Ignore());
             }
@@ -37,7 +37,7 @@ public class StructDescriptor {
             if (Object.class.equals(method.getDeclaringClass())) {
                 continue;
             }
-            StructProperty member = new StructProperty(method);
+            Prop member = new Prop(method);
             if (member.getAnnotation(Transient.class) != null && member.getAnnotation(DsonProperty.class) == null) {
                 Transient transientAnnotation = member.getAnnotation(Transient.class);
                 if (transientAnnotation.value()) {
@@ -45,6 +45,43 @@ public class StructDescriptor {
                 }
             }
             methods.put(method.getName(), member);
+        }
+    }
+
+    public static class Prop {
+
+        @DsonProperty("123")
+        public final Field field;
+        public final Method method;
+        public Encoder encoder;
+        public Decoder decoder;
+        private final Map<Class<? extends Annotation>, Annotation> annotations = new HashMap<>();
+        // shouldEncode can be used to omit empty value when encoding
+        public Predicate<Object> shouldEncode;
+
+        public Prop(Field field) {
+            this.field = field;
+            this.method = null;
+        }
+
+        public Prop(Method method) {
+            this.method = method;
+            this.field = null;
+        }
+
+        public <T extends Annotation> T getAnnotation(Class<T> clazz) {
+            Object annotation = annotations.get(clazz);
+            if (annotation != null) {
+                return (T) annotation;
+            }
+            if (field != null) {
+                return field.getAnnotation(clazz);
+            }
+            return method.getAnnotation(clazz);
+        }
+
+        public <T extends Annotation, A extends T> void setAnnotation(Class<T> clazz, A annotation) {
+            annotations.put(clazz, annotation);
         }
     }
 }
--- src/test/java/com/jsoniter/dson/codegen/StructDescriptorTest.java
+++ src/test/java/com/jsoniter/dson/codegen/StructDescriptorTest.java
@@ -1,6 +1,7 @@
 package com.jsoniter.dson.codegen;
 
 import com.jsoniter.dson.spi.DsonProperty;
+import com.jsoniter.dson.spi.StructDescriptor;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -30,13 +31,13 @@ public class StructDescriptorTest {
     @Test
     public void ignore_transient() {
         StructDescriptor struct = new StructDescriptor(MyClass.class);
-        StructProperty field1 = struct.fields.get("field1");
+        StructDescriptor.Prop field1 = struct.fields.get("field1");
         Assert.assertTrue(field1.getAnnotation(DsonProperty.class).ignore());
-        StructProperty field2 = struct.fields.get("field2");
+        StructDescriptor.Prop field2 = struct.fields.get("field2");
         Assert.assertFalse(field2.getAnnotation(DsonProperty.class).ignore());
-        StructProperty field3 = struct.methods.get("getField3");
+        StructDescriptor.Prop field3 = struct.methods.get("getField3");
         Assert.assertTrue(field3.getAnnotation(DsonProperty.class).ignore());
-        StructProperty field4 = struct.methods.get("getField4");
+        StructDescriptor.Prop field4 = struct.methods.get("getField4");
         Assert.assertFalse(field4.getAnnotation(DsonProperty.class).ignore());
     }
 }
